3aa68c5b9fbcdd481348f26becbe268fda23c6c1
zhanghaining0808 <2535708702@qq.com>
2023-06-17 01:18:13 +0800

    实现数据库连接 实现数据库基本存入 优化文件结构 添加文档 添加pymysql依赖项

---
+++ README.md
@@ -0,0 +1,9 @@
+运行该项目的步骤
+1 先到终端运行以下命令安装必要依赖项
+```base
+pip3 install -r dependencies.txt
+```
+2 运行以下命令启动项目
+```base
+python main.py
+```
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import questionary
+import db
+
 
 def main():
-    bank_list = [f"{bank}银行" for bank in  ["农业","工商","建设","交通"]]
-    now_select = questionary.select("choose your bank?",bank_list).ask()
-    print(now_select)
+    db.init_db()
+
 
 if __name__ == "__main__":
     main()
